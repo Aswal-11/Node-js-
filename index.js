8cfe3a18a7fdd37683888197a7a3041fc5b13d35
Aswal-11 <kartikaswal2001@gmail.com>
2025-10-14 01:00:02 +0530

    second commint

--- index.js
+++ index.js
@@ -5,49 +5,64 @@ app.listen(3000, () => {
   console.log("Connected on 3000");
 });
 
+app.set("view engine", 'ejs');
+
 app.get("/", (req, res) => {
-  res.send("hello ji");
+  // res.json({
+  //   name: "hello ji",
+  // });
+
+  const users = [
+    {
+      id:1, name:'rohan'
+    },
+    {
+      id:2, name:'mohan'
+    }
+  ]
+
+  res.json(users);
 });
 
-app.get("/about", (req, res) => {
-  res.send("this is about page");
+app.get("/about", (req, res) =>{
+  res.redirect(301,'https://www.google.com')
+})
+
+app.get("/blog", (req, res) =>{
+  res.send("blog page");
+})
+
+app.get("/contact", (req, res) =>{
+  res.send('this is contact page');
+})
+
+app.get("/user", (req, res) =>{
+  res.send('this is user page');
+})
+
+// this go back to user route because it always route to parent route 
+app.get("/user/userid/riad", (req, res) => {
+  res.redirect("..");
 });
 
-app.get("/gallery", (req, res) => {
-  res.send("this is about page");
-});
-
-app.get("/about/user", (req, res) => {
-  res.send("this is about user page");
-});
-
-// this will print the json value
-app.get("/user/:userid", (req, res) => {
-  res.send(req.params);
-});
-
-// nested route with mulitple params
+// http://localhost:3000/user/1/book/1
 app.get("/user/:userid/book/:bookid", (req, res) => {
-  // this will print the json value with both params
   res.send(req.params);
-
-  // this will print only bookid
-  // res.send(req.params.bookid);
-
-  // this will print only userid
-  // res.send(req.params.userid);
 });
 
-app.get("/product/:category-:productid", (req, res) => {
-    res.send(req.params);
-});
+// Throough this route we can render the html pages
+// command to install the ejs is "npm i ejs"
+app.get("/course", (req, res)=>{
+  res.render('course'); 
+})
 
-//put this link and see the results
-// http://localhost:3000/search?name=kartik&class=12th
+// we can download any file through this method 
+// whenever we open this link it will forcefully download the pdf
+app.get('/download', (req, res)=>{
+  res.download('./files/Kartik_aswal_resume.pdf', 'resume.pdf');
+})
 
-app.get("/search", (req, res) => {
-    let name = req.query.name;
-
-    let standard = req.query.class;
-    res.send("student name: "+ name + " standard is: "+ standard);
-});
+// we use this link it will open the resume in same in tab and dont let you to download the file forcefully 
+app.get('/resume', (req,res)=>{
+  res.sendFile(__dirname+ '/files/Kartik_aswal_resume.pdf')
+})
