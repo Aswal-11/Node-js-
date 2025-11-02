dedcab18e9ed4382bc5a0954d88fcb4e13c08d9a
Aswal-11 <kartikaswal2001@gmail.com>
2025-11-03 00:46:01 +0530

    update

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ app.get('/', (req, res)=>{
   res.render("home");
 })
 
-app.get('/show-contact', (req, res)=>{});
-app.get('/add-contact', (req, res)=>{});
-app.post('/add-contact', (req, res)=>{});
-app.get('/update-contact', (req, res)=>{});
-app.post('/post-contact', (req, res)=>{});
+app.get('/show-contact', (req, res)=>{res.render('show-contact')});
 
+app.get('/add-contact', (req, res)=>{res.render('add-contact')});
+app.post('/add-contact', (req, res)=>{});
+
+app.get('/update-contact', (req, res)=>{res.render('update-contact')});
+app.post('/update-contact', (req, res)=>{});
+
+app.get('/delete-contact', (req, res)=>{});
 
 app.listen(3000, () => {
   console.log('server successfully started on port number: 3000')
