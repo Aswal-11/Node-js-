bdda4e823acdc5b3c8d17ce3474ac17a2e322ee5
Aswal-11 <kartikaswal2001@gmail.com>
2026-01-29 10:25:11 +0530

    middleware applied

--- index.js
+++ index.js
@@ -1,8 +1,68 @@
 const express = require("express");
 const app = express();
 
+// This is the router middleware also we can say router based middleware
+const router = express.Router();
+
 app.listen(3000, () => {
   console.log("Connected on 3000");
+});
+
+/**
+ * This is the custom middleware
+ * It applies only when we declare them in routes
+ */
+// const middleware = ((req,res,next)=>{
+//   console.log(`${req.method} ${req.url}`);
+//   console.log("This is the first middleware");
+//   const d= new Date();
+//   console.log(`Time: ${d.getDate()} Date / ${d.getMonth()} month`)
+//   console.log(`Time: ${d.getHours()} hr / ${d.getMinutes()} min`)
+//   next();
+// });
+
+/**
+ * This is other middleware
+ */
+// const otherMiddleware = ((req, res, next)=>[
+//   console.log("This is the second middleware")
+// ]);
+
+// app.get("/", middleware, otherMiddleware, (req, res) => {
+//   res.send("hello ji");
+// });
+
+// app.get("/about",middleware, (req, res) => {
+//   res.send("this is about page");
+// });
+
+
+/**
+ * Router Level Middleware
+ */
+// router.use((req, res, next)=>{
+//   console.log("Router level Middleware");
+//   next();
+// })
+// router.get("/",(req, res) => {
+//   res.send("hello ji");
+// });
+
+// router.get("/about", (req, res) => {
+//   res.send("this is about page");
+// });
+
+// Here we have make the routes nested 
+// To access a route we have use this "/test" first
+// app.use("/test",router);
+
+
+/**
+ * Error Handling Middleware
+ */
+app.use((req, res, next)=>{
+  console.log("This is the second middleware")
+  next();
 });
 
 app.get("/", (req, res) => {
@@ -13,41 +73,11 @@ app.get("/about", (req, res) => {
   res.send("this is about page");
 });
 
-app.get("/gallery", (req, res) => {
-  res.send("this is about page");
-});
-
-app.get("/about/user", (req, res) => {
-  res.send("this is about user page");
-});
-
-// this will print the json value
-app.get("/user/:userid", (req, res) => {
-  res.send(req.params);
-});
-
-// nested route with mulitple params
-app.get("/user/:userid/book/:bookid", (req, res) => {
-  // this will print the json value with both params
-  res.send(req.params);
-
-  // this will print only bookid
-  // res.send(req.params.bookid);
-
-  // this will print only userid
-  // res.send(req.params.userid);
-});
-
-app.get("/product/:category-:productid", (req, res) => {
-    res.send(req.params);
-});
-
-//put this link and see the results
-// http://localhost:3000/search?name=kartik&class=12th
-
-app.get("/search", (req, res) => {
-    let name = req.query.name;
-
-    let standard = req.query.class;
-    res.send("student name: "+ name + " standard is: "+ standard);
+/**
+ * All the middlewares which are error baaed we have to place them all below the normal routes
+ */
+app.use((err, req, res, next)=>{
+  console.error(err);
+  res.status(500).send("Something is broken");
+  next();
 });
